6b4c1f3f49e8b20e102469c296d31283f5b8db7f
dashanWilliam <45125690+dashanWilliam@users.noreply.github.com>
2018-11-20 18:02:24 +0800

    Add files via upload

---
+++ js/checker.js
@@ -0,0 +1,13 @@
+var keys = ['a3sx', '102a'];
+
+
+$( document ).ready(function() {
+  // alert( "Thanks for visiting!" );
+  $("h1").click(function(event){
+    $(this).css('color', 'red');
+  });
+  $('html').click(function(){
+    alert('test');
+  });
+
+});
